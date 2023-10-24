6064a1b0a6f1b12d454ed3cab3e26617400abd52
Guilherme <guiramos027@gmail.com>
2023-10-23 21:46:01 -0300

    feat: add new routes

--- BrutePasta/BrutePastaApp/src/app/app-routing.module.ts
+++ BrutePasta/BrutePastaApp/src/app/app-routing.module.ts
@@ -2,8 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/products/product/product.component';
 import { GetProductsComponent } from './components/products/get-products/get-products.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
+	{
+		path: 'Home',
+		component: HomeComponent
+	},
+	{
+		path: 'Login',
+		component: LoginComponent
+	},
+	{
+		path: 'Register',
+		component: RegisterComponent
+	},
 	{
 		path: 'Product',
 		component: ProductComponent
