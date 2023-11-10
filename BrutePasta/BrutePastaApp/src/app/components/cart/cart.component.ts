2199dba766747365666d7c9617b2da75e6681d3b
Guilherme <guiramos027@gmail.com>
2023-11-10 18:49:53 -0300

    feat: add cart component

--- BrutePasta/BrutePastaApp/src/app/components/cart/cart.component.ts
+++ BrutePasta/BrutePastaApp/src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Item } from '../shared/item.model';
 import { CartService } from './shared/cart.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,17 +11,45 @@ import { CartService } from './shared/cart.service';
 export class CartComponent implements OnInit {
   cartItems?: Item[]
 
-  constructor( private cartService: CartService ) {
+  constructor( private cartService: CartService) {
+  }
+
+  calculateSubtotal(item: Item): number {
+    return item.quantity * item.product.price;
+  }
+
+  calculateTotal(): number {
+    const cartItems = this.cartService.getCart();
+    let total = 0;
+
+    for (const item of cartItems) {
+      total += this.cartService.calculateSubTotal(item);
+    }
+
+    return total + 20;
   }
 
   ngOnInit() {
     this.cartItems = this.cartService.getCart();
   }
 
-
-
   removeItem(item: Item) {
     this.cartService.removeFromCart(item);
+    this.cartItems = this.cartService.getCart();
+  }
+
+  removeAll(item: Item) {
+    this.cartService.removeAllFromCart(item);
+    this.cartItems = this.cartService.getCart();
+  }
+  
+  clearCart() {
+    this.cartService.clearCart();
+    this.cartItems = this.cartService.getCart();
+  }
+  
+  finalizeOrder() {
+    this.cartService.finalizeOrder();
     this.cartItems = this.cartService.getCart();
   }
 }
--- BrutePasta/BrutePastaApp/src/app/components/cart/shared/cart.service.ts
+++ BrutePasta/BrutePastaApp/src/app/components/cart/shared/cart.service.ts
@@ -1,14 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { Item } from '../../shared/item.model';
+import { Order } from '../../order/shared/order.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { OrderService } from '../../order/shared/order.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
+  @Input() order!: Order;
   cartItems: Item[] = [];
+  cpfForm!: FormGroup;
+  formBuilder!: FormBuilder;
+  
+
+  constructor( private orderService: OrderService) {
+    // Carregue os itens do carrinho do localStorage no construtor
+    const storedCart = localStorage.getItem('cartItems');
+
+    if (storedCart) {
+      this.cartItems = JSON.parse(storedCart);
+    }
+  }
+
+  ngOnInit() {
+    this.cpfForm = this.formBuilder.group({
+      cpf: [null],
+    })
+  }
 
   getCart(): Item[] {
     return this.cartItems;
+  }
+
+  calculateSubTotal(item: Item): number {
+    return item.quantity * item.product.price;
   }
 
   addToCart(item: Item) {
@@ -21,6 +47,8 @@ export class CartService {
       // Caso contrário, adicione o novo item
       this.cartItems.push(item);
     }
+
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   removeFromCart(item: Item) {
@@ -32,9 +60,56 @@ export class CartService {
         this.cartItems.splice(index, 1);
       }
     }
+
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
+  removeAllFromCart(item: Item) {
+    const index = this.cartItems.indexOf(item);
+    if (index !== -1) {
+      this.cartItems.splice(index, 1);
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   clearCart() {
     this.cartItems = [];
+
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
+  finalizeOrder() {
+    const order: Order = {
+      items: this.getCart(),
+      payment: {
+        paymentMethod: {
+          id: 1, // Defina o método de pagamento desejado
+          name: ''
+        },
+      },
+      client: {
+        id: 2, // Defina o cliente ou outras informações necessárias
+        cpf: '',
+        name: '',
+        email: '',
+        password: '',
+        phoneNumber: ''
+      },
+      deliveryMan: {
+        id: 0,
+        cpf: '',
+        name: '',
+        orderTax: 0
+      }
+    }
+    console.log(order);
+    this.orderService.createOrder(order).subscribe((response) => {
+      console.log("Deu boa caralho")
+      this.clearCart();
+      // this.router.navigate(['/confirmation-page']);
+    });
+    this.clearCart();
+    localStorage.removeItem('cartItems');
   }
 }
