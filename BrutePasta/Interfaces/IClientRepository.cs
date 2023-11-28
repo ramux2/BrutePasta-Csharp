ae56e56fccd899f19847b69160d9af409b6860b1
Guilherme <guiramos027@gmail.com>
2023-11-27 21:43:12 -0300

    refactor: create Client Interface

---
+++ BrutePasta/Interfaces/IClientRepository.cs
@@ -0,0 +1,14 @@
+﻿using BrutePasta.Models;
+
+namespace BrutePasta.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<Client> GetClientById(int? id);
+        Task<Client> GetClientByEmail(string? email);
+        Task<Client> GetClientByCpf(string? cpf);
+        Task<Client> Create(Client client);
+        Task<Client> Update(Client client);
+        Task<Client> Remove(Client client);
+    }
+}
